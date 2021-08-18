4bbc711658c329a1ad03d10b6d1fe3c6eadf5598
douglasmatosdev <douglasmatosdev@gmail.com>
2021-08-18 13:04:45 -0300

    convert from class component to function component

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,40 +1,34 @@
 import React from "react";
 import "./index.css";
 
-class Counter extends React.Component {
-    state = {
-        counter: 0
-    };
+function Counter() {
+    const [counter, setCounter] = React.useState(0);
 
-    getModifier() {
-        if (this.state.counter > 0) return "counter__title--increment";
-        if (this.state.counter < 0) return "counter__title--decrement";
+    function getModifier() {
+        if (counter > 0) return "counter__title--increment";
+        if (counter < 0) return "counter__title--decrement";
         return "";
     }
 
-    render() {
-        const { counter } = this.state
-
-        return (
-            <div className="counter">
-                <h1 className={`counter__title ${this.getModifier()}`}>{counter}</h1>
-                <section className="buttons">
-                    <button
-                        onClick={() => this.setState({ counter: counter + 1 })}
-                        className="button button--increment"
-                    >
-                        incrementar
-                    </button>
-                    <button
-                        onClick={() => this.setState({ counter: counter - 1 })}
-                        className="button button--decrement"
-                    >
-                        decrementar
-                    </button>
-                </section>
-            </div>
-        );
-    };
+    return (
+        <div className="counter">
+            <h1 className={`counter__title ${getModifier()}`}>{counter}</h1>
+            <section className="buttons">
+                <button
+                    onClick={() => setCounter(counter + 1)}
+                    className="button button--increment"
+                >
+                    incrementar
+                </button>
+                <button
+                    onClick={() => setCounter(counter - 1)}
+                    className="button button--decrement"
+                >
+                    decrementar
+                </button>
+            </section>
+        </div>
+    );
 }
 
 export default Counter;
